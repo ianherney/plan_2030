09ebdfa02f072932de15c70f568d6f483a449d11
ianherney <62864215+ianherney@users.noreply.github.com>
2020-04-01 14:24:32 -0500

    v1 | inical

---
+++ codes.js
@@ -0,0 +1,88 @@
+// generic ------------------------------------------------
+
+document.oncontextmenu = function(){return false}
+
+
+// code_clima ---------------------------------------------
+
+$(document).ready(function(){
+    $("#estrategia_1").click(function(){
+        $("#data_estrategia_1").slideToggle("slow");
+        $("#est_2").slideToggle("slow");
+        $("#est_3").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#estrategia_2").click(function(){
+        $("#data_estrategia_2").slideToggle("slow");
+        $("#est_1").slideToggle("slow");
+        $("#est_3").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#estrategia_3").click(function(){
+        $("#data_estrategia_3").slideToggle("slow");
+        $("#est_1").slideToggle("slow");
+        $("#est_2").slideToggle("slow");
+    });
+});
+
+// code_comunidad 7 estrategias----------------------------
+
+$(document).ready(function(){
+    $("#com_est_1").click(function(){
+        $("#data_est_com_1").slideToggle("slow");
+        $("#com_0").slideToggle("slow");
+        $("#com_2").slideToggle("slow");
+        $("#com_3").slideToggle("slow");
+        $("#com_4").slideToggle("slow");
+        $("#com_5").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#com_est_2").click(function(){
+        $("#data_est_com_2").slideToggle("slow");
+        $("#com_0").slideToggle("slow");
+        $("#com_1").slideToggle("slow");
+        $("#com_3").slideToggle("slow");
+        $("#com_4").slideToggle("slow");
+        $("#com_5").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#com_est_3").click(function(){
+        $("#data_est_com_3").slideToggle("slow");
+        $("#com_0").slideToggle("slow");
+        $("#com_1").slideToggle("slow");
+        $("#com_2").slideToggle("slow");
+        $("#com_4").slideToggle("slow");
+        $("#com_5").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#com_est_4").click(function(){
+        $("#data_est_com_4").slideToggle("slow");
+        $("#com_0").slideToggle("slow");
+        $("#com_1").slideToggle("slow");
+        $("#com_2").slideToggle("slow");
+        $("#com_3").slideToggle("slow");
+        $("#com_5").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#com_est_5").click(function(){
+        $("#data_est_com_5").slideToggle("slow");
+        $("#com_0").slideToggle("slow");
+        $("#com_1").slideToggle("slow");
+        $("#com_2").slideToggle("slow");
+        $("#com_3").slideToggle("slow");
+        $("#com_4").slideToggle("slow");
+    });
+});
